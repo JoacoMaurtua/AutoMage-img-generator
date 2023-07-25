73e106896b277424a155b68184e718490025ecc9
JoacoMaurtua <joacomaurtua@gmail.com>
2023-07-25 11:16:51 -0500

    Image generator working

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 //Configuracion:
 const configuration = new Configuration({
-  apiKey: process.env.OPENAIA_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY, 
 });
 
 const openia = new OpenAIApi(configuration);
@@ -35,7 +35,7 @@ router.route('/').post(async (req,res)=>{
 
     const image = aiResponse.data.data[0].b64_json;
 
-    res.status(200).json({photo:image});
+   res.status(200).json({ photo:image });
   }catch(error){
     console.log(error);
     res.status(500).send(error?.response.data.error.message)
