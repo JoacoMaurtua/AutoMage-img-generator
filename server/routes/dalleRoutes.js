e9a0aad437913a2ab8051c3c62ca18eab9074058
JoacoMaurtua <joacomaurtua@gmail.com>
2023-07-25 00:13:56 -0500

    generate image controller/post

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,8 +16,31 @@ const openia = new OpenAIApi(configuration);
 
 //Endpoints:
 
-router.get('/').get((req,res)=>{
+//Prueba
+router.get('/', (req,res)=>{
   res.send('Hello from DALL-E')
 })
+
+//Crear una imagen
+router.route('/').post(async (req,res)=>{
+  try{
+    const {prompt} = req.body;
+
+    const aiResponse = await openia.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({photo:image});
+  }catch(error){
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message)
+  }
+});
+
 
 export default router;
