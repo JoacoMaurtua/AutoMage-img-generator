cb47b667d820e4694b78041e835b3de29095e8ee
JoacoMaurtua <joacomaurtua@gmail.com>
2023-07-26 11:02:54 -0500

    AI generator v1 finished

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,8 +17,11 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState([]);
   const [searchText, setSearchText] = useState('');
-
-  //Desplegar todos los posts:
+  const [searchedResults, setSearchedResults] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  
+  
+ //Desplegar todos los posts:
  
   const fetchPosts = async () => {
     setLoading(true);
@@ -48,6 +51,21 @@ const Home = () => {
     fetchPosts();
   }, []);
 
+  //Funcion para la busqueda de Posts
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase()
+          .includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
+    );
+  };
+
+
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -59,7 +77,14 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <FormField/>
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -78,7 +103,7 @@ const Home = () => {
               <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                 {searchText?(
                   <RenderCards
-                    data={[]}
+                    data={searchedResults}
                     title="No search results found"
                   />
                 ):(
